c3e14bd3c70530b78fd8e2f5d0be5c75b44fe838
farosia <380642234@qq.com>
2018-07-20 13:23:20 +0800

    7.20 homecoming

--- WEB/jsd_project/scripts/global.js
+++ WEB/jsd_project/scripts/global.js
@@ -278,6 +278,107 @@ function displayAbbreviations() {
   container.appendChild(dlist);
 }
 
+function focusLables() {
+  if (!document.getElementsByTagName) return false;
+  var labels = document.getElementsByTagName("label");
+  for (var i=0; i<labels.length; i++) {
+    if (!labels[i].getAttribute("for")) continue;
+    labels[i].onclick = function() {
+      var id = this.getAttribute("for");
+      if (!document.getElementById(id)) return false;
+      var element = document.getElementById(id);
+      element.focus();
+    }
+  }
+}
+
+function resetFields(whichform) {
+  // if (Modernizr.input.placeholder) return;
+  for (var i=0; i<whichform.elements.length; i++) {
+    var element = whichform.elements[i];
+    if (element.type == "submit") continue;
+    var check = element.placeholder || element.getAttribute('placeholder');
+    if (!check) continue;
+    element.onfocus = function() {
+      var text = this.placeholder || this.getAttribute('placeholder');
+      if (this.value == text) {
+        this.className = '';
+        this.value = "";
+      }
+    }
+    element.onblur = function() {
+      if (this.value == "") {
+        this.className = 'placeholder';
+        this.value = this.placeholder || this.getAttribute('placeholder');
+      }
+    }
+    element.onblur();
+  }
+}
+
+function prepareForms() {
+  for (var i=0; i<document.forms.length; i++) {
+    var thisform = document.forms[i];
+    resetFields(thisform);
+    thisform.onsubmit = function() {
+      return validateForm(this);
+    }
+  }
+}
+
+function isFilled(field) {
+  if (field.value.replace(' ','').length == 0) return false;
+  var placeholder = field.placeholder || field.getAttribute('placeholder');
+  return (field.value != placeholder);
+}
+
+function isEmail(field) {
+  return (field.value.indexOf("@") != -1 && field.value.indexOf(".") != -1);
+}
+
+function validateForm(whichform) {
+  for (var i=0; i<whichform.elements.length; i++) {
+    var element = whichform.elements[i];
+    if (element.required == true) {
+      if (!isFilled(element)) {
+        alert("Please fill in the "+element.name+"field.");
+        return false;
+      }
+    }
+    if (element.type == 'email') {
+      if (!isEmail(element)) {
+        alert("The "+element.name+" field must be a valid email address.");
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+function getHTTPObject() {
+  if (typeof XMLHttpRequest == "undefined")
+    XMLHttpRequest = function(){
+      try {return new ActiveXObject("Msxml2.XMLHTTP.6.0");}
+        catch (e) {}
+      try {return new ActiveXObject("Msxml2.XMLHTTP.3.0");}
+        catch (e) {}
+      try {return new ActiveXObject("Msxml2.XMLHTTP");}
+        catch (e) {}
+      return false;
+    }
+    return new XMLHttpRequest();
+}
+
+function displayAjaxLoading(element) {
+  while (element.hasChildNodes()) {
+    element.removeChild(element.lastChild);
+  }
+  var content = document.createElement("img");
+  content.setAttribute("src","images/loading.gif");
+  content.setAttribute("alt","Loading...");
+  element.appendChild(content);
+}
+
 addLoadEvent(highlightPage);
 addLoadEvent(prepareSlideshow);
 addLoadEvent(prepareInternalnav);
@@ -286,3 +387,5 @@ addLoadEvent(prepareGallery);
 addLoadEvent(StripeTables);
 addLoadEvent(highlightRows);
 addLoadEvent(displayAbbreviations);
+addLoadEvent(focusLables);
+addLoadEvent(prepareForms);
