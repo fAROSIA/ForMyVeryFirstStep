b9768e2cbbf07241bcf187b57c92a36648bfd879
farosia <380642234@qq.com>
2018-08-16 18:10:46 +0800

    8.16

--- Python/goForActivity/main.py
+++ Python/goForActivity/main.py
@@ -1,4 +1,4 @@
 import func
 func.get181('todayMatch.txt', '今日大赛.xml')
 func.get181('readyToGo.txt', '随来随打.xml')
-func.buildALot('ID-11.txt', 'LID-11.txt', 'raw.txt')
+func.buildALot('ID--11.txt', 'LID--11.txt', 'raw.txt')
