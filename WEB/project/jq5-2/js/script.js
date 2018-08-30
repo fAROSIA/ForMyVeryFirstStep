0476ab5529d6ed005a95501774161bfeccd235d9
farosia <380642234@qq.com>
2018-08-30 18:29:23 +0800

    8.30

--- WEB/project/jq5-2/js/script.js
+++ WEB/project/jq5-2/js/script.js
@@ -16,9 +16,13 @@ function hideLayer() {
   $('#layer-mask').hide();
   // 隐藏弹出层窗体
   $('#layer-pop').hide();
+  // 清除表单内容
+  $('.signIn-layer :input').not(':button').val('');
+  $('.signUp-layer :input').not(':button').val('');
+  $('.err-msg').html('');
 }
 // 弹出登陆框
-function showSignin() {
+function showSignIn() {
   $('.signIn-btn').addClass('btn-active');
   $('.signUp-btn').removeClass('btn-active');
   $('.signIn-layer').show();
@@ -27,18 +31,18 @@ function showSignin() {
   showLayer();
   //登陆表单校验
   $('.account').blur(function () {
-    if ($(this).val().length != 11 || isNaN(($(this).val()))) {
-      $('.error-msg1').html("请输入正确的邮箱或手机号");
+    if ($(this).val() !== '' && $(this).val().length != 11 || isNaN(($(this).val()))) {
+      $('.error-msg1').html('请输入正确的邮箱或手机号');
     } else {
-      $('.error-msg1').html("");
+      $('.error-msg1').html('');
     }
   });
   $('.passwd').blur(function () {
     var passwd = $(this).val();
-    if (passwd.length < 6 || passwd.length > 16) {
-      $('.error-msg2').html("请输入6-16位密码，区分大小写，不能用空格");
+    if (passwd !== '' && passwd.length < 6 || passwd.length > 16) {
+      $('.error-msg2').html('请输入6-16位密码，区分大小写，不能用空格');
     } else {
-      $('.error-msg2').html("");
+      $('.error-msg2').html('');
     }
   });
 }
@@ -52,37 +56,100 @@ function showSignUp() {
   showLayer();
   // 注册表单校验
   $('.account-signUp').blur(function () {
-    if ($(this).val().length != 11 || isNaN(($(this).val()))) {
-      $('.error-msg3').html("请输入正确的邮箱或手机号");
+    if ($(this).val() !== '' && $(this).val().length != 11 || isNaN(($(this).val()))) {
+      $('.error-msg3').html('请输入正确的邮箱或手机号');
     } else {
-      $('.error-msg3').html("");
+      $('.error-msg3').html('');
     }
   });
   $('.verifywd').blur(function () {
-    if ($(this).val() != "GYyd") {
-      $('.error-msg4').html("验证码错误，区分大小写");
+    if ($(this).val() !== '' && $(this).val() != 'GYyd') {
+      $('.error-msg4').html('验证码错误，区分大小写');
     } else {
-      $('.error-msg4').html("");
+      $('.error-msg4').html('');
     }
   });
 }
+
+// ui-slider
+$.fn.UiSlider = function () {
+  var ui = $(this);
+  var wrap = $('.ui-slider-wrap');
+  var btn_prev = $('.ui-slider-arrow .left', ui);
+  var btn_next = $('.ui-slider-arrow .right', ui);
+  var items = $('.ui-slider-wrap .item', ui);
+  var tips = $('.ui-slider-process .item', ui);
+  var current = 0;
+  var size = items.size();
+  var width = items.eq(0).width();
+  var enableAuto = true;
+  // 鼠标移动到图片上不滚动
+  ui
+    .on('mouseover', function () {
+      enableAuto = false;
+    })
+    .on('mouseout', function () {
+      enableAuto = true;
+    });
+  // 具体操作
+  wrap
+    .on('move_prev', function () {
+      if (current <= 0) {
+        current = size;
+      }
+      current -= 1;
+      wrap.triggerHandler('move_to', current);
+    })
+    .on('move_next', function () {
+      if (current >= size - 1) {
+        current = -1;
+      }
+      current += 1;
+      wrap.triggerHandler('move_to', current);
+    })
+    .on('move_to', function (evt, index) {
+      wrap.css('left', index * width * (-1));
+      tips.removeClass('item_focus').eq(index).addClass('item_focus');
+    })
+    .on('auto_move', function () {
+      setInterval(function () {
+        enableAuto && wrap.triggerHandler('move_next');
+      }, 2000);
+    })
+    .triggerHandler('auto_move');
+
+  // 事件
+  btn_prev.on('click', function () {
+    wrap.triggerHandler('move_prev');
+  });
+  btn_next.on('click', function () {
+    wrap.triggerHandler('move_next');
+  });
+  tips.on('click', function () {
+    var index = $(this).index();
+    wrap.triggerHandler('move_to', index);
+  });
+};
+
+// 回到顶部
+$.fn.uiBackTop = function () {
+  $('.back-to-top').on('click', function () {
+    $(window).scrollTop(0);
+  });
+};
 
 $(function () {
   // 下次自动登录默认选中
   ($('#auto-signin')).prop('checked', true);
   // 点击登陆、注册
   $('.signIn-btn').on('click', function () {
-    showSignin();
+    showSignIn();
   });
   $('.signUp-btn').on('click', function () {
     showSignUp();
   });
-  // top右边栏
-  $('.top-right-list').on('mouseover',function () {
-    $(this).children(".top-right-list-item").addClass('top-active');
-  });
-  $('.top-right-list').on('mouseout',function () {
-    $(this).children(".top-right-list-item").removeClass('top-active');
-  });
-  
+  // 轮播图
+  $('.ui-slider').UiSlider();
+  // 回到顶部
+  $('.right-nav').uiBackTop();
 });
