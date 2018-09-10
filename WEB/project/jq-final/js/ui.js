f319b01cfaa735859787eafefb71e9a1a3c8c1af
farosia <380642234@qq.com>
2018-09-10 17:58:16 +0800

    9.10

--- WEB/project/jq-final/js/ui.js
+++ WEB/project/jq-final/js/ui.js
@@ -15,8 +15,21 @@ $.fn.uiSearch = function () {
     $('.ui-search-select-list').hide();
   });
 };
-
+// tab切换
+$.fn.uiTabSwitch = function () {
+  $('.tab-item').on('click', function () {
+    var index = $(this).index();
+    $(this).siblings().removeClass('tab-active');
+    $(this).addClass('tab-active');
+    $('.main-bottom-content').hide().eq(index).show();
+  });
+};
 // 页面的脚本逻辑
 $(function () {
-	$('.ui-search').uiSearch();
+  // 搜索栏
+  $('.ui-search').uiSearch();
+  // tab切换
+  // 默认展示预约挂号tab
+  $('.main-bottom-content').eq(0).show();
+  $('.tab').uiTabSwitch();
 });
